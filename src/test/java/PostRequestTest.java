50bc313d8cf9df603fd4e58e47284f88c9cc8438
Tanny <lynxy86@mail.ru>
2024-08-08 10:56:10 +0300

    Initial commit

---
+++ src/test/java/PostRequestTest.java
@@ -0,0 +1,27 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostRequestTest {
+
+    @Test
+    public void testPostRequest() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"));
+
+    }
+}
+
